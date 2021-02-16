c3a4b99420b05a3076afbf6aaaa4f88104a0cdf4
Gaia Cravesana <gcravesana@gmail.com>
2021-02-16 12:38:18 +0000

    implemented the index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #CRUD
+
+  #C
+
+  #R List all tasks
+  get "tasks", to: "tasks#index"
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  #U
+
+
+  #D
+
 end
